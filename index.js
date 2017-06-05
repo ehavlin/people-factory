b9801096eae7436b8c45b75425a39b7a0f48bef3
Evan Havlin <ehavlin@hotmail.com>
2017-06-05 17:16:00 -0400

    Change color of paragraph based on input

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    const color = document.querySelector('#emptyParagraph')
-    color.textContent = 'This color: ' + f.personColor.value
-    heading.textContent = f.personName.value + "'s favorite color is " + f.personColor.value
+    const emptyParagrah = document.querySelector('#emptyParagraph')
+    const color = f.personColor.value
+    heading.textContent = f.personName.value + "'s favorite color is " + color
+    emptyParagraph.textContent = 'This color'
+    emptyParagraph.style.color = color
 }
 
 personForm.addEventListener('submit', handleSubmit)
