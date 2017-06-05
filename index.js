621fcdd04e9c68dc03fd0191bb9a69d57dad75ac
Evan Havlin <ehavlin@hotmail.com>
2017-06-05 17:04:00 -0400

    Add paragraph that states entered color.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
+    const color = document.querySelector('#emptyParagraph')
+    color.textContent = 'This color: ' + f.personColor.value
     heading.textContent = f.personName.value + "'s favorite color is " + f.personColor.value
 }
 
