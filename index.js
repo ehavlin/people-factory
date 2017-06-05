139187a979d7c48878ce3bcfbb8ec0e0385c032c
Evan Havlin <ehavlin@hotmail.com>
2017-06-05 16:57:46 -0400

    Add color text box and update heading with it.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value
+    heading.textContent = f.personName.value + "'s favorite color is " + f.personColor.value
 }
 
 personForm.addEventListener('submit', handleSubmit)
