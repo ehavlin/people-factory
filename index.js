add5ae5bfbb82330294fd511b679695fcc82ab55
Evan Havlin <ehavlin@hotmail.com>
2017-06-06 14:05:48 -0400

    Add name to details

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const  personForm = document.querySelector('#personForm')
 function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
+    const details = document.querySelector('#details0')
+    const name = f.personName.value
+    details.innerHTML += '<strong>' + name + '</strong>'
+
+    /** 
     const heading = document.querySelector('h1')
     const emptyParagrah = document.querySelector('#emptyParagraph')
     const color = f.personColor.value
     heading.textContent = f.personName.value + "'s favorite color is " + color
     emptyParagraph.textContent = 'This color'
     emptyParagraph.style.color = color
+    */
 }
 
 personForm.addEventListener('submit', handleSubmit)
