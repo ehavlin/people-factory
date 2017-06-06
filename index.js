0ca913229ff67ef7262438640ce10fa022649873
Evan Havlin <ehavlin@hotmail.com>
2017-06-06 16:27:32 -0400

    Make list in separate functions

--- index.js
+++ index.js
@@ -1,68 +1,46 @@
-const  personForm = document.querySelector('#personForm')
+const personForm = document.querySelector('#personForm')
+
+function renderColor(color) {
+  const div = document.createElement('div')
+  div.style.backgroundColor = color
+  div.style.width = '100px'
+  div.style.height = '50px'
+
+  return div
+}
+
+function renderListItem(label, value) {
+  const item = document.createElement('li')
+  item.textContent = `${label}: ${value}`
+
+  return item
+}
+
+function renderList(personData) {
+  const list = document.createElement('ul')
+
+  // ['name', 'favoriteColor', 'age']
+  Object.keys(personData).map(function(label) {
+    const item = renderListItem(label, personData[label])
+    list.appendChild(item)
+  })
+
+  return list
+}
 
 function handleSubmit(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const details = document.querySelector('#details')
-    const name = f.personName.value
-    const favoriteColor = f.favoriteColor.value
-    const age = f.age.value
+  ev.preventDefault()
+  const f = ev.target
+  const details = document.querySelector('#details')
 
-    // const colorDiv = `
-    // <div style="background-color: ${favoriteColor}; width: 100px; height: 50px;"></div>
-    // `
-
-    // details.innerHTML = `
-    //     <ul>
-    //         <li>Name: ${name}</li>
-    //         <li>Favorie Color: ${colorDiv}</li>
-    //         <li>Age: ${age}</li>
-    //     </ul>
-    //     `
-    
-    addToDetails(name, favoriteColor, age)
-
-
-
-    // const boldedName = document.createElement('strong')
-    // boldedName.textContent = name
-
-    // details.appendChild(boldedName)
-
-    /** 
-    const heading = document.querySelector('h1')
-    const emptyParagrah = document.querySelector('#emptyParagraph')
-    const color = f.personColor.value
-    heading.textContent = f.personName.value + "'s favorite color is " + color
-    emptyParagraph.textContent = 'This color'
-    emptyParagraph.style.color = color
-    */
-}
-
-function addToDetails(name, favoriteColor, age){
-    const unOrderedList = document.createElement('ul')
-    const listName = document.createElement('li')
-    listName.textContent = `Name: ${name}`
-
-    const listFavoriteColor = document.createElement('li')
-    listFavoriteColor.innerHTML = `Favorite Color: ${renderColor(favoriteColor).outerHTML}`
-
-    const listAge = document.createElement('li')
-    listAge.textContent = `Age: ${age}`
-   
-    details.appendChild(unOrderedList)
-    unOrderedList.appendChild(listName)
-    unOrderedList.appendChild(listFavoriteColor)
-    unOrderedList.appendChild(listAge)
-}
-
-function renderColor(color){
-    const div = document.createElement('div')
-    div.style.backgroundColor = color
-    div.style.width = '100px'
-    div.style.height = '50px'
-
-    return div
+  const person = {
+    name: f.personName.value,
+    favoriteColor: f.favoriteColor.value,
+    age: f.age.value,
+  }
+  
+  const list = renderList(person)
+  details.appendChild(list)
 }
 
 personForm.addEventListener('submit', handleSubmit)
